bc0201b6c0de871f740c15f6db125de46e3d83c4
sonnguyen-logigear <son.nguyen@logigear.com>
2017-05-05 15:32:10 +0700

    Update BuildSizeChange.java

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/BuildSizeChange.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/BuildSizeChange.java
@@ -181,7 +181,7 @@ public class BuildSizeChange {
 
     final StringBuilder comment = new StringBuilder();
 
-    comment.append("Artifact size changes PAKA:\n");
+    comment.append("Artifact size changes PAKA 2:\n");
     for (Map.Entry<String, SizeChange> e : artChanges.entrySet()) {
       String changeStr;
       if (e.getValue() != null) {
